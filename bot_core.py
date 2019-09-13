9c77c63da093be9ec2a80079ca1658ce81b2563d
Michael Salamakha <Wolf2195@gmail.com>
2019-09-13 20:07:05 +0300

    added some functionality

--- bot_core.py
+++ bot_core.py
@@ -2,16 +2,19 @@ import discord
 import asyncio
 import requests
 
+from discord import Game
+
 DISCORD_BOT_TOKEN = ''
 
 client = discord.Client()
 
+BTC_PRICE_URL_coinmarketcap = 'https://api.coinmarketcap.com/v1/ticker/bitcoin/?convert=RUB'
+
+
 @client.event
 async def on_ready():
     print("I'm up!")
-    await client.change_presence(game=discord.Game(name='Just Chilling'), #with live wires
-                                 status=discord.Status('idle'),
-                                 afk=True)
+    await client.change_presence(activity=discord.Game("Just Chilling"))
 
 
 @client.event
@@ -20,7 +23,37 @@ async def on_message(message):
     if message.author == client.user:
         return
     if message.content == "hello":
-        await channel.message(" ^_^ ")
-        # await client.send_message(message.channel, " ^_^")      
+        await message.channel.send(' ^_^')
+
+    if message.content == 'user_list':
+        for user in client.users:
+            await message.channel.send(user)
+
+    
+    if message.content.startswith('$thumb'):
+        channel = message.channel
+        await channel.send('Send me that 👍 reaction, mate')
+
+        def check(reaction, user):
+            return user == message.author and str(reaction.emoji) == '👍'
+
+        try:
+            reaction, user = await client.wait_for('reaction_add', timeout=60.0, check=check)
+        except asyncio.TimeoutError:
+            await channel.send('👎')
+        else:
+            await channel.send('👍')
+
+    if message.content.startswith('!btcprice'):
+        print('[command]: btcprice ')
+        btc_price_usd, btc_price_rub = get_btc_price()
+        await message.channel.send( 'USD: ' + str(btc_price_usd) + ' | RUB: ' + str(btc_price_rub))
+
+def get_btc_price():
+    r = requests.get(BTC_PRICE_URL_coinmarketcap)
+    response_json = r.json()
+    usd_price = response_json[0]['price_usd']
+    rub_rpice = response_json[0]['price_rub']
+    return usd_price, rub_rpice
 
 client.run(DISCORD_BOT_TOKEN)
